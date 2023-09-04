ed1719df18eebb42ba510ba4c9849f96739bae6b
Ceren <109301665+cerenkuru@users.noreply.github.com>
2023-09-04 18:50:05 +0300

    Add files via upload

---
+++ src/components/navi/Navi.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  
+} from 'reactstrap';
+import CartSummary from '../cart/CartSummary';
+
+function Navi(args) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar {...args}>
+        <NavbarBrand href="/">Northwind</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+              </NavLink>
+            </NavItem>
+            <CartSummary/>
+          </Nav>
+          
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Navi;
